767717d7a5364628ffda435c4f5ab525603e2f3e
AurelioMsFilho <Aureliofilhotj@gmail.com>
2021-09-15 18:43:43 -0300

    commit inicial

---
+++ js/main.js
@@ -0,0 +1,91 @@
+
+
+
+
+
+
+
+function soma(n1,n2){
+    return n1 + n2;
+}
+function setReplace(frase, nome, novo_nome){
+    return fraase.replace(nome, novo_nome)
+}
+alert (soma(5, 10));
+
+alert(setReplace( "vai Japão", "Japão", "Brasil"));
+
+/*var d= new Date();
+alert (d);
+alert(d.getMonth()+1);
+alert(d.getMinutes());
+alert(d.getHours());
+*/
+/*
+var count;
+for(count =0; count<=5; count++) {
+    console.log(count);
+   //alert(count);
+};
+*/
+/*
+var count=0;
+while (count <=5) {
+    //console.log(count);
+    alert(count);
+   //count = count + 1; ou
+   count++;
+};*/
+/*
+var idade = prompt ("Qual a sua idade?: ")
+//var idade = 18
+if(idade>=18){
+    alert ("maior de idade");
+}
+    else{
+        alert ("menor de idade");
+    };
+*/
+/*
+var frutas = [{nome:"maçã", cor: "vermelha"}, {nome: "uva", cor: " roxa "}]
+console.log(frutas);
+alert(frutas[1].nome);
+*/
+/*
+var fruta={nome:"maçã", cor: "vermelha"};
+console.log(fruta.nome);
+alert(fruta.cor);
+*/
+
+//var lista=[" maça ", " pêra ", " laranja "];
+//lista.push(" uva");
+//lista.pop();
+//console.log(lista.reverse());
+//console.log (lista.length);
+
+
+
+//console.log(lista)
+//console.log(lista[0]);
+//console.log(lista.toString()[2]);
+//console.log(lista.toString())
+//console.log(lista.join("-"));
+
+//alert(lista);
+//var nome = "Aurélio Filho"; 
+//var idade = 39;
+//var idade2 = 10;
+//var frase= "Japão melhor time do mundo "
+ //var n1=5
+ //var n2=3
+ //var media= (n1+n2)/2
+
+//alert("Bem vindo "+ nome + " ao Curso JS. Sabemos que você tem " + idade + " de idade");
+//alert(idade +idade2)
+//alert(frase.toUpperCase ("Japão","brasil"))
+//console.log(nome)
+//console.log(idade + idade2)
+//console.log((idade2)+2)
+//console.log(frase.replace ("Japão","Brasil"))
+//console.log(" A soma de 5 + 3 é = " + (n1+n2))
+//console.log("e a Média é = " + (media))
